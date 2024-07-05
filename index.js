98fef041ee3e744b3fb26304830768e65d263d98
Alexander Karanja <alexanderkaranja78@gmail.com>
2024-07-05 22:27:31 +0300

    dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let mainElement = document.querySelector('main#main');
+if (mainElement) {
+    mainElement.remove();
+}
+ 
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
+document.body.appendChild(newHeader);
